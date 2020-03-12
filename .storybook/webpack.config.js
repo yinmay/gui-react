d1bf6818952ab25071d41acecd461bdebb6bce12
yinmei <yinmei@decobim.com>
2020-03-12 19:00:29 +0800

    添加svgloader

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,17 +1,19 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = ({ config }) => {
-
-  config.module.rules.push({
-    test: /\.(ts|tsx)$/,
-    loaders: ['awesome-typescript-loader', 'react-docgen-typescript-loader'],
-    include: path.resolve(__dirname, '../'),
-  },
-      {
-        test: /\.scss$/,
-        loaders: ['style-loader', 'css-loader', 'sass-loader'],
-        include: path.resolve(__dirname, '../'),
-      });
-  config.resolve.extensions.push(".ts", ".tsx",".scss");
+  config.module.rules.push(
+    {
+      test: /\.(ts|tsx)$/,
+      loaders: ['awesome-typescript-loader', 'react-docgen-typescript-loader'],
+      include: path.resolve(__dirname, '../')
+    },
+    { test: /\.svg$/, loader: 'svg-sprite-loader' },
+    {
+      test: /\.scss$/,
+      loaders: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../')
+    }
+  );
+  config.resolve.extensions.push('.ts', '.tsx', '.scss');
   return config;
 };
--- src/Icon/icon.tsx
+++ src/Icon/icon.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './button.scss';
+import wechat from '../../statics/wechat.svg';
+
 interface IProps {
   /**
    * Name for the button
@@ -8,6 +10,8 @@ interface IProps {
 }
 
 const Icon: React.FunctionComponent<IProps> = (props: IProps) => {
+  console.log(wechat);
+
   return <a className='f6 link dim br3 ph3 pv2 mb2 dib white bg-light-purple test'>{props.name}</a>;
 };
 
